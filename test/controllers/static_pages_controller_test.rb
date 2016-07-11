3c40e04fca621e1337b1f82277a179eaa20e5087
Stefan Babic <babic.stefan@gmail.com>
2016-07-11 19:02:08 +0200

    add title to pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Test Application"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Test Application"
+
   end
 
   test "should get about" do
   	get static_pages_about_url
   	assert_response :success
+    assert_select "title", "About | Test Application"
+
   end
 
 end
