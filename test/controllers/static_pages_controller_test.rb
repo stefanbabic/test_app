1c362ee36b47b15df62e2e049f6e42c507b06605
Stefan Babic <babic.stefan@gmail.com>
2016-07-11 20:18:54 +0200

    create contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -26,4 +26,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   end
 
+  test "should get contact" do
+  	get static_pages_contact_url
+  	assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+
+  end
+
 end
