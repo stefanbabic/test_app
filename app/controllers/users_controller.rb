441e94d494a5ca942e5d61d9554610a72f518836
Stefan Babic <babic.stefan@gmail.com>
2016-07-19 00:06:57 +0200

    require logged-in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -12,7 +13,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
   	  log_in @user
-      flash[:success] = "Herzlich Willkommen, #{@user.name}"
+      flash[:success] = "Herzlich Willkommen, #{@user.name}!"
       redirect_to @user
   	else
   		render 'new'
@@ -39,4 +40,11 @@ class UsersController < ApplicationController
   		params.require(:user).permit(:name, :email, :password,
   									 :password_confirmation)
   	end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Bitte zuerst anmelden oder registrieren."
+        redirect_to login_url
+      end
+    end
 end
