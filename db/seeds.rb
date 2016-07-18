97880dfea1bc30cf9b475b7519dd331207776cb5
Stefan Babic <babic.stefan@gmail.com>
2016-07-19 01:45:06 +0200

    add admin attribute

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 User.create!(name:  "Fake User",
              email: "example@test.org",
              password:              "testing",
-             password_confirmation: "testing")
+             password_confirmation: "testing",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
