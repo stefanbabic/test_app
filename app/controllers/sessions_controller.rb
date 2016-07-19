eed9de7a267ce3d9da390059ce5781742877499b
Stefan Babic <babic.stefan@gmail.com>
2016-07-19 20:32:09 +0200

    add account activations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-  		log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		redirect_back_or user
+      if user.activated?
+  		  log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  		  redirect_back_or user
+      else
+        message = "Konto nicht aktiviert."
+        message += "Überprüfen Sie Ihre E-Mail für den Aktivierungslink."
+        flash[:warning] = message
+        redirect_to root_url
+      end
   	else
   		flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  	  UserMailer.account_activation(@user).deliver_now
+  	  @user.send_activation_email
       flash[:info] = "Bitte überprüfen Sie Ihre E-Mail-Adresse."
       redirect_to root_url
   	else
