d5f4f463727e439d8f9130a9874be9223d0ab2d2
Stefan Babic <babic.stefan@gmail.com>
2016-07-20 00:18:17 +0200

    add create action for password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,19 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Ich habe Ihnen für die Änderung Ihres Passwortes einen Link gesendet."
+      redirect_to root_url
+    else
+      flash.now[:danger] = "E-Mail Adresse nicht gefunden"
+      render 'new'
+    end
+  end
+
   def edit
   end
 end
