fb0605ce0619282befecd77be4093d5851f6f7ce
Stefan Babic <babic.stefan@gmail.com>
2016-07-21 11:50:43 +0200

    temporary hide help link

--- test/integration/layout_test.rb
+++ test/integration/layout_test.rb
@@ -6,7 +6,7 @@ class LayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
+    #assert_select "a[href=?]", help_path
     assert_select "a[href=?]", "/web/viewer.html?file=lebenslauf.pdf"
     assert_select "a[href=?]", contact_path
   end
